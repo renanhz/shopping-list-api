a923afd4a1bc542aef4d51f5fc91cd7a1a8f3465
renan <renan.stos077@gmail.com>
2024-04-29 17:37:12 -0300

    initial commit

---
+++ src/test/java/com/example/shoppinglistapi/ShoppingListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppinglistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
