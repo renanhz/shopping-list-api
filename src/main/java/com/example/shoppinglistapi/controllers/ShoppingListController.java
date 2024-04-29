8308dcbd4bb1d7ba797d1e0f9193e246e86694ac
renan <renan.stos077@gmail.com>
2024-04-29 17:57:25 -0300

    ShoppingListController hello world

---
+++ src/main/java/com/example/shoppinglistapi/controllers/ShoppingListController.java
@@ -0,0 +1,16 @@
+package com.example.shoppinglistapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/shopping-list")
+public class ShoppingListController {
+
+    @RequestMapping("/{channelId}")
+    public ResponseEntity<String> getShoppingList(@PathVariable("channelId") String channelId) {
+        return ResponseEntity.ok("Hello World");
+    }
+}
