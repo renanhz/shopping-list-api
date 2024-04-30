2051abe26cc278d51d59b6f10fab8a4d91caee4e
renan <renan.stos077@gmail.com>
2024-04-30 14:48:23 -0300

    ShoppingListRepository findByChannelId method

--- src/main/java/com/example/shoppinglistapi/shoppinglist/ShoppingListRepository.java
+++ src/main/java/com/example/shoppinglistapi/shoppinglist/ShoppingListRepository.java
@@ -1,6 +1,12 @@
 package com.example.shoppinglistapi.shoppinglist;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
+
+    @Query("SELECT s FROM shopping_list s WHERE s.channel_id = ?1 AND s.closed = 0")
+    Optional<ShoppingList> findByChannelId(Long channelId);
 }
