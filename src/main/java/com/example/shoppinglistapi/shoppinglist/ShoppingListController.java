4d61e1dfc4b7f9eed2662ef2018b05b7754718e8
renan <renan.stos077@gmail.com>
2024-05-02 14:30:32 -0300

    getMapping getShoppingList

--- src/main/java/com/example/shoppinglistapi/shoppinglist/ShoppingListController.java
+++ src/main/java/com/example/shoppinglistapi/shoppinglist/ShoppingListController.java
@@ -1,6 +1,7 @@
 package com.example.shoppinglistapi.shoppinglist;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/shopping-list")
 public class ShoppingListController {
 
-    @RequestMapping("/{channelId}")
+    @GetMapping("/{channelId}")
     public ResponseEntity<String> getShoppingList(@PathVariable("channelId") Long channelId) {
         return ResponseEntity.ok("Hello World");
     }
