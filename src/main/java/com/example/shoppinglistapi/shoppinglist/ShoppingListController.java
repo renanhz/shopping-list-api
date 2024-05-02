168a2ba17a612d67a397fdddfcd6f2ada8e3d93a
renan <renan.stos077@gmail.com>
2024-05-02 15:20:52 -0300

    post shoppingList

--- src/main/java/com/example/shoppinglistapi/shoppinglist/ShoppingListController.java
+++ src/main/java/com/example/shoppinglistapi/shoppinglist/ShoppingListController.java
@@ -1,17 +1,30 @@
 package com.example.shoppinglistapi.shoppinglist;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/shopping-list")
 public class ShoppingListController {
 
+    @Autowired
+    private ShoppingListService shoppingListService;
+
+    public ShoppingListController(ShoppingListService shoppingListService) {
+        this.shoppingListService = shoppingListService;
+    }
+
     @GetMapping("/{channelId}")
     public ResponseEntity<String> getShoppingList(@PathVariable("channelId") Long channelId) {
         return ResponseEntity.ok("Hello World");
+    }
+
+    @PostMapping("/{channelId}")
+    public ResponseEntity<ShoppingList> createShoppingList(@PathVariable("channelId") Long channelId) {
+        ShoppingList shoppingList = shoppingListService.createShoppingList(channelId);
+
+        return new ResponseEntity<ShoppingList>(shoppingList, HttpStatus.CREATED);
     }
 }
