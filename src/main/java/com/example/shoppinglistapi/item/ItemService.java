fe7ea4d8a2ef40386f9bbbbd158436fd29ae6e16
renan <renan.stos077@gmail.com>
2024-05-15 12:00:44 -0300

    ItemService creation

---
+++ src/main/java/com/example/shoppinglistapi/item/ItemService.java
@@ -0,0 +1,15 @@
+package com.example.shoppinglistapi.item;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+}
