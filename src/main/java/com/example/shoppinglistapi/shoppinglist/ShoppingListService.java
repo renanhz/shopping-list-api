c6cd2666fabff0e2360527ca094e4cb7947897fa
renan <renan.stos077@gmail.com>
2024-05-15 11:37:05 -0300

    createShoppingList closing old list

--- src/main/java/com/example/shoppinglistapi/shoppinglist/ShoppingListService.java
+++ src/main/java/com/example/shoppinglistapi/shoppinglist/ShoppingListService.java
@@ -3,6 +3,7 @@ package com.example.shoppinglistapi.shoppinglist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -21,6 +22,13 @@ public class ShoppingListService {
 
     public ShoppingList createShoppingList(Long channelId) {
         ShoppingList shoppingList = new ShoppingList(channelId);
+
+        getOpenShoppingList(channelId).ifPresent(sl -> {
+            sl.setClosed(true);
+            sl.setClosedDate(LocalDate.now());
+
+            shoppingListRepository.save(sl);
+        });
 
         return shoppingListRepository.save(shoppingList);
     }
