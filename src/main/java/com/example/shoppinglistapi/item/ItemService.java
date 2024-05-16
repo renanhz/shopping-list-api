85412c39344b4633c318d68b9926b4a6035c6f99
renan <renan.stos077@gmail.com>
2024-05-16 10:10:41 -0300

    ItemService addItemToList

--- src/main/java/com/example/shoppinglistapi/item/ItemService.java
+++ src/main/java/com/example/shoppinglistapi/item/ItemService.java
@@ -1,17 +1,38 @@
 package com.example.shoppinglistapi.item;
 
+import com.example.shoppinglistapi.shoppinglist.ShoppingList;
+import com.example.shoppinglistapi.shoppinglist.ShoppingListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ItemService {
 
+
+
     @Autowired
     ItemRepository itemRepository;
 
-    public ItemService(ItemRepository itemRepository) {
+    @Autowired
+    ShoppingListService shoppingListService;
+
+    public ItemService(ItemRepository itemRepository, ShoppingListService shoppingListService) {
         this.itemRepository = itemRepository;
+        this.shoppingListService = shoppingListService;
     }
 
+    public void addItemToList(Long channelId, Item item) {
+        Optional<ShoppingList> shoppingList = shoppingListService.getOpenShoppingList(channelId);
+
+        if (shoppingList.isPresent()) {
+            item.setListId(shoppingList.get().getId());
+
+            itemRepository.save(item);
+        } else {
+//            throw new Exception("É preciso criar uma lista antes de adicionar um item");
+        }
+    }
 
 }
