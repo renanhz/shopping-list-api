98298912d2856d88aa89d47f25dc7d9174aebc03
renan <renan.stos077@gmail.com>
2024-05-20 15:14:33 -0300

    shoppingListDTO

---
+++ src/main/java/com/example/shoppinglistapi/shoppinglist/ShoppingListDTO.java
@@ -0,0 +1,19 @@
+package com.example.shoppinglistapi.shoppinglist;
+
+import com.example.shoppinglistapi.item.Item;
+import com.example.shoppinglistapi.item.ItemDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingListDTO {
+
+    private List<ItemDTO> items = new ArrayList<>();
+
+    public ShoppingListDTO(ShoppingList sl) {
+        for (Item item:sl.getItems()) {
+            ItemDTO itemDTO = new ItemDTO(item);
+            items.add(itemDTO);
+        }
+    }
+}
